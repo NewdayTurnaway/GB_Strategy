f8b12cf4892a726bacce876fc8b1d38fc6faf0a5
NewdayTurnaway <moba545@gmail.com>
2022-08-27 20:07:37 +0300

    Update to Lesson_1

---
+++ Assets/_Root/Scripts/Abstractions/ISelectable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Abstractions
+{
+    public interface ISelectable
+    {
+        Health Health { get; }
+        Sprite Icon { get; }
+    }
+}
---
+++ Assets/_Root/Scripts/Core/MainBuilding.cs
@@ -0,0 +1,34 @@
+using Abstractions;
+using UnityEngine;
+
+namespace Core
+{
+    public sealed class MainBuilding : MonoBehaviour, IUnitProducer, ISelectable
+    {
+        public Health Health => _health;
+        public Sprite Icon => _icon;
+
+        [Header("Unit Produce Settings")]
+        [SerializeField] private GameObject _unitPrefab;
+        [SerializeField] private Transform _unitsParent;
+
+        [Header("Build Settings")]
+        [SerializeField] private Health _health;
+        [SerializeField] private float _startHealth;
+        [SerializeField] private Sprite _icon;
+
+        private void Awake()
+        {
+            _health.SetMaxToCurrent();
+            _health.CurrentHealth = _startHealth;
+        }
+
+        public void ProduceUnit()
+        {
+            Instantiate(_unitPrefab,
+                new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)),
+                Quaternion.identity,
+                _unitsParent);
+        }
+    }
+}
