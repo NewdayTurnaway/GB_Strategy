a81fe29f7767baa2b559eb63e650cfac8a6fe3ad
NewdayTurnaway <moba545@gmail.com>
2022-09-17 20:17:46 +0300

    Add delay to produce units

--- Assets/_Root/Scripts/Core/CommandExecutors/ProduceUnitCommandExecutor.cs
+++ Assets/_Root/Scripts/Core/CommandExecutors/ProduceUnitCommandExecutor.cs
@@ -1,5 +1,6 @@
 using Abstractions.Commands;
 using Abstractions.Commands.CommandsInterfaces;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Core
@@ -8,13 +9,26 @@ namespace Core
     {
         [Header("Unit Produce Settings")]
         [SerializeField] private Transform _unitsParent;
+        [SerializeField] private int _timeDelayToProduce = 3000;
 
-        public override void ExecuteSpecificCommand(IProduceUnitCommand command)
+        private int _queueNumber = 0;
+
+        public override async void ExecuteSpecificCommand(IProduceUnitCommand command)
         {
+            if (_queueNumber < 0)
+            {
+                _queueNumber = 0;
+            }
+            _queueNumber++;
+            Debug.Log($"{name} | Produce Unit: InProcess | Queue Number: {_queueNumber}");
+            await Task.Delay(_queueNumber * _timeDelayToProduce);
             Instantiate(command.UnitPrefab,
                         new Vector3(Random.Range(-10, 10), 0.5f, Random.Range(-10, 10)),
                         Quaternion.identity,
                         _unitsParent);
+            
+            _queueNumber--;
+            Debug.Log($"{name} | Produce Unit: Complete | Queue Number: {_queueNumber}");
         }
     }
 }
