ef01ba0ef2bb29d0bc19aa37fe529b62ea4d6a18
NewdayTurnaway <moba545@gmail.com>
2022-09-17 14:46:35 +0300

    Add AwaiterBase

--- Assets/_Root/Scripts/Abstractions/NewValueNotifier.cs
+++ Assets/_Root/Scripts/Abstractions/NewValueNotifier.cs
@@ -1,46 +1,21 @@
-﻿using System;
-using Utils;
+﻿using Utils;
 
 namespace Abstractions
 {
-    public sealed class NewValueNotifier<TAwaited> : IAwaiter<TAwaited>
+    public sealed class NewValueNotifier<TAwaited> : AwaiterBase<TAwaited>
     {
         private readonly ValueBase<TAwaited> _valueBase;
-        private TAwaited _result;
-        
-        private bool _isCompleted;
-        private Action _continuation;
-        
-        public bool IsCompleted => _isCompleted;
 
         public NewValueNotifier(ValueBase<TAwaited> valueBase)
         {
             _valueBase = valueBase;
             _valueBase.OnNewValue += OnNewValue;
         }
-        
-        public TAwaited GetResult() => 
-            _result;
-
-
-        public void OnCompleted(Action continuation)
-        {
-            if (_isCompleted)
-            {
-                continuation?.Invoke();
-            }
-            else
-            {
-                _continuation = continuation;
-            }
-        }
 
         private void OnNewValue(TAwaited obj)
         {
             _valueBase.OnNewValue -= OnNewValue;
-            _result = obj;
-            _isCompleted = true;
-            _continuation?.Invoke();
+            OnWaitFinish(obj);
         }
     }
 }
--- Assets/_Root/Scripts/Core/StopAwaiter.cs
+++ Assets/_Root/Scripts/Core/StopAwaiter.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using Utils;
+﻿using Utils;
 
 namespace Core
 {
-    public class StopAwaiter : IAwaiter<AsyncExtensions.Void>
+    public class StopAwaiter : AwaiterBase<AsyncExtensions.Void>
     {
         private readonly UnitMovementStop _unitMovementStop;
-        private Action _continuation;
-        private bool _isCompleted;
-
-        public bool IsCompleted => _isCompleted;
 
         public StopAwaiter(UnitMovementStop unitMovementStop)
         {
@@ -17,26 +12,10 @@ namespace Core
             _unitMovementStop.OnStop += OnStop;
         }
 
-        public AsyncExtensions.Void GetResult() =>
-            new();
-
-        public void OnCompleted(Action continuation)
-        {
-            if (_isCompleted)
-            {
-                continuation?.Invoke();
-            }
-            else
-            {
-                _continuation = continuation;
-            }
-        }
-
         private void OnStop()
         {
             _unitMovementStop.OnStop -= OnStop;
-            _isCompleted = true;
-            _continuation?.Invoke();
+            OnWaitFinish(new AsyncExtensions.Void());
         }
     }
 }
