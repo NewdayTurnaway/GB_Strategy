e595de6b8af9dde28d132ac25099ba46b9ed4681
NewdayTurnaway <moba545@gmail.com>
2022-09-18 15:07:02 +0300

    Update to Lesson_5

---
+++ Assets/_Root/Scripts/UserControlSystem/UI/Presenter/MenuPresenter.cs
@@ -0,0 +1,21 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UserControlSystem.UI.Presenter
+{
+    public class MenuPresenter : MonoBehaviour
+    {
+        [SerializeField] private Button _backButton;
+        [SerializeField] private Button _exitButton;
+
+        private void Start()
+        {
+            _backButton.OnClickAsObservable().Subscribe(_ => gameObject.SetActive(false));
+#if UNITY_EDITOR
+            _exitButton.OnClickAsObservable().Subscribe(_ => UnityEditor.EditorApplication.isPlaying = false);
+#endif
+            _exitButton.OnClickAsObservable().Subscribe(_ => Application.Quit());
+        }
+    }
+}
