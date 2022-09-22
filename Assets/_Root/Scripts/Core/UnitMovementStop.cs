4a1c5aea09f9d896598f693c5e512eaf3c213d21
NewdayTurnaway <moba545@gmail.com>
2022-09-22 14:46:29 +0300

    Add CollisionDetector

--- Assets/_Root/Scripts/Core/UnitMovementStop.cs
+++ Assets/_Root/Scripts/Core/UnitMovementStop.cs
@@ -1,20 +1,55 @@
-﻿using System;
+﻿using Abstractions;
+using System;
+using UniRx;
 using UnityEngine;
 using UnityEngine.AI;
 using Utils;
 
 namespace Core
 {
-    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(NavMeshAgent), typeof(CollisionDetector))]
     public sealed class UnitMovementStop : MonoBehaviour, IAwaitable<AsyncExtensions.Void>
     {
         [SerializeField] private NavMeshAgent _agent;
-        
+        [SerializeField] private CollisionDetector _collisionDetector;
+        [SerializeField] private int _throttleFrames = 60;
+        [SerializeField] private int _continuityThreshold = 10;
+
         public event Action OnStop;
 
-        private void OnValidate() => 
+        private void OnValidate()
+        {
             _agent ??= GetComponent<NavMeshAgent>();
+            _collisionDetector ??= GetComponent<CollisionDetector>();
+        }
 
+        private void Awake()
+        {
+            _collisionDetector.Collisions
+                .Where(_ => _agent.hasPath)
+                .Where(collision => collision.collider.GetComponentInParent<IUnit>() != null)
+                .Select(_ => Time.frameCount)
+                .Distinct()
+                .Buffer(_throttleFrames)
+                .Where(buffer =>
+                {
+                    for (int i = 1; i < buffer.Count; i++)
+                    {
+                        if (buffer[i] - buffer[i - 1] > _continuityThreshold)
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                })
+                .Subscribe(_ =>
+                {
+                    _agent.isStopped = true;
+                    _agent.ResetPath();
+                    OnStop?.Invoke();
+                })
+                .AddTo(this);
+        }
 
         private void Update()
         {
