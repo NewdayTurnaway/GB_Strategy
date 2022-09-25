20875beeca2161aa40d4f5a3f37ec1fc99364e04
NewdayTurnaway <moba545@gmail.com>
2022-09-25 18:01:41 +0300

    Fix, refactoting AttackCommandExecutor

---
+++ Assets/_Root/Scripts/Core/AttackOperationAwaiter.cs
@@ -0,0 +1,21 @@
+﻿using Utils;
+
+namespace Core
+{
+    public class AttackOperationAwaiter : AwaiterBase<AsyncExtensions.Void>
+    {
+        private readonly AttackOperation _attackOperation;
+
+        public AttackOperationAwaiter(AttackOperation attackOperation)
+        {
+            _attackOperation = attackOperation;
+            attackOperation.OnComplete += OnComplete;
+        }
+
+        private void OnComplete()
+        {
+            _attackOperation.OnComplete -= OnComplete;
+            OnWaitFinish(new AsyncExtensions.Void());
+        }
+    }
+}
--- Assets/_Root/Scripts/Core/CommandExecutors/AttackCommandExecutor.cs
+++ Assets/_Root/Scripts/Core/CommandExecutors/AttackCommandExecutor.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 using Abstractions;
 using Abstractions.Commands.CommandsInterfaces;
 using UniRx;
 using UnityEngine;
 using UnityEngine.AI;
-using Utils;
 using Zenject;
 
 namespace Core
@@ -15,6 +13,7 @@ namespace Core
     [RequireComponent(typeof(Animator), typeof(StopCommandExecutor))]
     public sealed class AttackCommandExecutor : CommandExecutorBase<IAttackCommand>
     {
+        [SerializeField] private Transform _transform;
         [SerializeField] private NavMeshAgent _navMeshAgent;
         [SerializeField] private Animator _animator;
         [SerializeField] private StopCommandExecutor _stopCommandExecutor;
@@ -25,21 +24,28 @@ namespace Core
 
         private Vector3 _ourPosition;
         private Vector3 _targetPosition;
-        private Quaternion _ourRotation;
 
         private Transform _targetTransform;
         private AttackOperation _attackOperation;
 
         private readonly Subject<Vector3> _targetPositions = new();
-        private readonly Subject<Quaternion> _targetRotations = new();
         private readonly Subject<IAttackable> _attackTargets = new();
 
         private static readonly int _attackAnimation = Animator.StringToHash("Attack");
         private static readonly int _walkAnimation = Animator.StringToHash("Walk");
         private static readonly int _idleAnimation = Animator.StringToHash("Idle");
 
+        public IHealthHolder OurHealth => _ourHealth;
+        public float AttackingDistance => _attackingDistance;
+        public int AttackingPeriod => _attackingPeriod;
+        public Vector3 OurPosition => _ourPosition;
+        public Vector3 TargetPosition => _targetPosition;
+        public Subject<Vector3> TargetPositions => _targetPositions;
+        public Subject<IAttackable> AttackTargets => _attackTargets;
+
         private void OnValidate()
         {
+            _transform = transform;
             _navMeshAgent = GetComponent<NavMeshAgent>();
             _animator = GetComponent<Animator>();
             _stopCommandExecutor = GetComponent<StopCommandExecutor>();
@@ -59,12 +65,6 @@ namespace Core
             _attackTargets
                 .ObserveOnMainThread()
                 .Subscribe(StartAttackingTargets);
-
-            _targetRotations
-                .ObserveOnMainThread()
-                .Subscribe(SetAttackRotation);
-
-            //UpdatePosition(); // if enabled - application freeze
         }
 
         private void Update()
@@ -73,20 +73,16 @@ namespace Core
             {
                 return;
             }
-            //Has time to work only in debug mode, after setting a breakpoint in the stream and pressing the F5 button
+
             UpdatePosition();
         }
 
         private void UpdatePosition()
         {
-            //lock (this)
+            _ourPosition = _transform.position;
+            if (_targetTransform != null)
             {
-                _ourPosition = transform.position;
-                _ourRotation = transform.rotation;
-                if (_targetTransform != null)
-                {
-                    _targetPosition = _targetTransform.position;
-                }
+                _targetPosition = _targetTransform.position;
             }
         }
 
@@ -94,33 +90,21 @@ namespace Core
         {
             _targetTransform = (command.Target as Component).transform;
             _attackOperation = new(this, command.Target);
-            //UpdatePosition(); // if enabled - work only StartMovingToPosition | if disabled - work SetAttackRotation & StartMovingToPosition
-            _stopCommandExecutor.CancellationTokenSource = new();
+            _attackOperation.StartAttackAlgorithm();
+            UpdatePosition();
+            await _stopCommandExecutor.ExecuteOtherCommandWithCancellation(_attackOperation, CancelCommand);
+        }
 
-            try
-            {
-                await _attackOperation.WithCancellation(_stopCommandExecutor.CancellationTokenSource.Token);
-            }
-            catch
-            {
-                _attackOperation.Cancel();
-            }
-
+        private void CancelCommand()
+        {
+            _attackOperation.Cancel();
             _animator.SetTrigger(_idleAnimation);
             _attackOperation = null;
             _targetTransform = null;
-            _stopCommandExecutor.CancellationTokenSource = null;
-        }
-
-        private void SetAttackRotation(Quaternion targetRotation)
-        {
-            Debug.Log(nameof(SetAttackRotation));
-            transform.rotation = targetRotation;
         }
 
         private void StartAttackingTargets(IAttackable target)
         {
-            Debug.Log(nameof(StartAttackingTargets));
             _navMeshAgent.isStopped = true;
             _navMeshAgent.ResetPath();
             _animator.SetTrigger(_attackAnimation);
@@ -129,104 +113,8 @@ namespace Core
 
         private void StartMovingToPosition(Vector3 position)
         {
-            Debug.Log(nameof(StartMovingToPosition));
             _navMeshAgent.destination = position;
             _animator.SetTrigger(_walkAnimation);
         }
-
-        #region AttackOperation
-
-        public sealed class AttackOperation : IAwaitable<AsyncExtensions.Void>
-        {
-            public class AttackOperationAwaiter : AwaiterBase<AsyncExtensions.Void>
-            {
-                private readonly AttackOperation _attackOperation;
-
-                public AttackOperationAwaiter(AttackOperation attackOperation)
-                {
-                    _attackOperation = attackOperation;
-                    attackOperation.OnComplete += OnComplete;
-                }
-
-                private void OnComplete()
-                {
-                    _attackOperation.OnComplete -= OnComplete;
-                    OnWaitFinish(new AsyncExtensions.Void());
-                }
-            }
-
-            private event Action OnComplete;
-
-            private readonly AttackCommandExecutor _attackCommandExecutor;
-            private readonly IAttackable _target;
-
-            private bool _isCancelled;
-
-            public AttackOperation(AttackCommandExecutor attackCommandExecutor, IAttackable target)
-            {
-                _target = target;
-                _attackCommandExecutor = attackCommandExecutor;
-
-                var thread = new Thread(AttackAlgorythm);
-                thread.Start();
-            }
-
-            public void Cancel()
-            {
-                _isCancelled = true;
-                OnComplete?.Invoke();
-            }
-
-            private void AttackAlgorythm(object obj)
-            {
-                while (true) // Breakpoint
-                {
-                    if (_attackCommandExecutor == null
-                        || _attackCommandExecutor._ourHealth.Health.CurrentHealth == 0
-                        || _target.Health.CurrentHealth == 0
-                        || _isCancelled)
-                    {
-                        OnComplete?.Invoke();
-                        return;
-                    }
-
-                    var targetPosition = default(Vector3);
-                    var ourPosition = default(Vector3);
-                    var ourRotation = default(Quaternion);
-                    //lock (_attackCommandExecutor)
-                    {
-                        //
-                        targetPosition = _attackCommandExecutor._targetPosition;
-                        ourPosition = _attackCommandExecutor._ourPosition;
-                        ourRotation = _attackCommandExecutor._ourRotation;
-                    }
-
-                    var vector = targetPosition - ourPosition;
-                    var distanceToTarget = vector.magnitude;
-                    if (distanceToTarget > _attackCommandExecutor._attackingDistance)
-                    {
-                        var finalDestination = targetPosition
-                            - vector.normalized
-                            * (_attackCommandExecutor._attackingDistance * 0.9f);
-                        _attackCommandExecutor._targetPositions.OnNext(finalDestination);
-                        Thread.Sleep(100);
-                    }
-                    else if (ourRotation != Quaternion.LookRotation(vector))
-                    {
-                        _attackCommandExecutor._targetRotations.OnNext(Quaternion.LookRotation(vector));
-                    }
-                    else
-                    {
-                        _attackCommandExecutor._attackTargets.OnNext(_target);
-                        Thread.Sleep(_attackCommandExecutor._attackingPeriod);
-                    }
-                }
-            }
-
-            public IAwaiter<AsyncExtensions.Void> GetAwaiter() =>
-                new AttackOperationAwaiter(this);
-        }
-
-        #endregion
     }
 }
